547a1ae26d46379b518413fb87cec91636f3165e
MUSHLIHUN <muslihun@untan.ac.id>
2023-01-02 18:11:50 +0700

    first commit

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data=array(
+            array(
+                'name'=>'Admin',
+                'email'=>'admin@gmail.com',
+                'password'=>Hash::make('1111'),
+                'role'=>'admin',
+                'status'=>'active'
+            ),
+            array(
+                'name'=>'User',
+                'email'=>'user@gmail.com',
+                'password'=>Hash::make('1111'),
+                'role'=>'user',
+                'status'=>'active'
+            ),
+            array(
+                'name'=>'listcreative',
+                'email'=>'listcreative15@gmail.com',
+                'password'=>Hash::make('qwer1234'),
+                'role'=>'admin',
+                'status'=>'active'
+            ),
+        );
+
+        DB::table('users')->insert($data);
+    }
+}
